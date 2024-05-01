efbed87c2b5b7db20aeb2daad37ae9f94b09bc86
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-05-01 10:52:09 +0000

    Add onClick event handler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithPro
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
       {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
       {/* <StatefulGreeting greeting="I'm a stateful class component" name="Mike" /> */}
       {/* <StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Mike" /> */}
-      <StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Mike" />
+      {/* <StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Mike" /> */}
+      <EventsFunctional />
+      <EventsClass />
     </div>  
   );
 }
