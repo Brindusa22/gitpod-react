0e221e9c101d68fac84b1359b0f16bb777863c0e
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-05-25 12:55:47 +0000

    Add empty array as second parameter to the useEffect hook

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -4,6 +4,8 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] =  useState(0);
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
         console.log('count 1 effect');
         document.title = count;
@@ -13,6 +15,12 @@ function UseEffectCounter() {
         console.log('count 2 effect');
         document.title = count2;
     }, [count2]);
+    useEffect(() => {
+        console.log('Creating timer')
+        const interval = setInterval(() => {
+            setTime(time => time + 1);
+        }, 1000);
+    }, []);
   return (
     <div>
         <button onClick={
@@ -25,9 +33,11 @@ function UseEffectCounter() {
         }>
                 Increment Count 2({count2})
         </button>
+        <h2>Time is {time}</h2>
 
     </div>
   )
 }
 
 export default UseEffectCounter
+
